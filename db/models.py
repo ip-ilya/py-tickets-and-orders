6be3c00e346d51671a3bf84ae61f2023bf310dfe
Illia Khomutov <ilyakhomutov613@gmail.com>
2025-03-11 16:03:36 +0200

    Solution 2

--- db/models.py
+++ db/models.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
 from django.db import models
 
+import settings
+
 
 class Genre(models.Model):
     name = models.CharField(max_length=255, unique=True)
@@ -56,13 +58,17 @@ class MovieSession(models.Model):
 
 class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey("User", on_delete=models.CASCADE)
-
-    class Meta:
-        ordering = ["-created_at"]
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="orders"
+    )
 
     def __str__(self) -> str:
         return f"{self.created_at}"
+
+    class Meta:
+        ordering = ["-created_at"]
 
 
 class Ticket(models.Model):
