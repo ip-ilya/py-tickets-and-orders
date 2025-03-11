3045a88d2c1e82cbaa0fa68fd7f27cea63bdb753
Illia Khomutov <ilyakhomutov613@gmail.com>
2025-03-12 01:12:46 +0200

    Solution 2

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,4 +1,5 @@
 from django.db.models import QuerySet
+from django.shortcuts import get_object_or_404
 
 from db.models import MovieSession
 
@@ -21,7 +22,7 @@ def get_movies_sessions(session_date: str = None) -> QuerySet:
 
 
 def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
-    return MovieSession.objects.get(id=movie_session_id)
+    return get_object_or_404(MovieSession, pk=movie_session_id)
 
 
 def update_movie_session(
@@ -30,7 +31,7 @@ def update_movie_session(
     movie_id: int = None,
     cinema_hall_id: int = None,
 ) -> None:
-    movie_session = MovieSession.objects.get(id=session_id)
+    movie_session = get_movie_session_by_id(session_id)
 
     if show_time:
         movie_session.show_time = show_time
@@ -42,7 +43,7 @@ def update_movie_session(
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
-    MovieSession.objects.get(id=session_id).delete()
+    get_movie_session_by_id(session_id).delete()
 
 
 def get_taken_seats(movie_session_id: int) -> list[dict]:
